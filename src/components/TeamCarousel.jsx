6e9955508de96c147f32b6732058fad5b1a4aa35
Therealblackgold <gauta.ncholo@gmail.com>
2023-06-22 00:52:42 +0200

    ULS first commit

--- src/components/TeamCarousel.jsx
+++ src/components/TeamCarousel.jsx
@@ -2,6 +2,7 @@ import { Col, Row } from "react-bootstrap";
 import USER from "../assets/dp.png";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import Image from "next/image";
 
 const TeamCarousel = () => {
   const responsive = {
@@ -34,43 +35,43 @@ const TeamCarousel = () => {
           >
             {/* slider item 1 */}
             <div className="item">
-              <img src={USER} alt="Team" />
+              <Image src={USER} alt="Upper Level Security Team" />
               <h5>Tumelo Mokoena</h5>
               <span>Operations Manager</span>
             </div>
             {/* slider item 2 */}
             <div className="item">
-              <img src={USER} alt="Team" />
+              <Image src={USER} alt="Upper Level Security Team" />
               <h5>Richard Mohau Mokoena</h5>
               <span>Managing Director</span>
             </div>
             {/* slider item 3 */}
             <div className="item">
-              <img src={USER} alt="Team" />
+              <Image src={USER} alt="Upper Level Security Team" />
               <h5>Jerry Mokoena</h5>
               <span>Tactical Operations</span>
             </div>
             {/* slider item 4 */}
             <div className="item">
-              <img src={USER} alt="Team" />
+              <Image src={USER} alt="Upper Level Security Team" />
               <h5>Thabang Mzizi</h5>
               <span>Human Resource Management</span>
             </div>
             {/* slider item 5 */}
             <div className="item">
-              <img src={USER} alt="Team" />
+              <Image src={USER} alt="Upper Level Security Team" />
               <h5>Sydney Gumede</h5>
               <span>Sales & Marketing</span>
             </div>
             {/* slider item 6 */}
             <div className="item">
-              <img src={USER} alt="Team" />
+              <Image src={USER} alt="Upper Level Security Team" />
               <h5>Tumi Nkosi </h5>
               <span>Financial Administration</span>
             </div>
             {/* slider item 7 */}
             <div className="item">
-              <img src={USER} alt="Team" />
+              <Image src={USER} alt="Upper Level Security Team" />
               <h5>Dorah Nkosi</h5>
               <span>Administration</span>
             </div>
